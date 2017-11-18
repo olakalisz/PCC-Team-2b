b075aa01c42728508d876d0f385c0a8ba33b94c6
Victor <victorwtang@yahoo.com>
2017-11-18 14:01:21 -0800

    Added pixelate filter

--- Web Cam Face Detection.py
+++ Web Cam Face Detection.py
@@ -3,6 +3,15 @@
 import numpy as np
 import cv2
 
+
+def pixelate(frame):
+    height, width, channels = frame.shape
+
+    frame = cv2.resize(frame, (width // 32, height // 32), interpolation=cv2.INTER_NEAREST)
+    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_NEAREST)
+    return frame
+
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
@@ -13,10 +22,10 @@ while 1:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
+        # cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        # roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        
+        img[y:y + h, x:x + w] = pixelate(roi_color)
         #eyes = eye_cascade.detectMultiScale(roi_gray)
         #for (ex,ey,ew,eh) in eyes:
         #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
